7e75f413ecdd6ddc41b06970002041c2b44eb4f9
Robert Gruendler <robert@dubture.com>
2011-11-25 21:13:28 +0100

    added Twig_ExtensionInterface

--- app/code/local/Symfony/Core/Block/Template.php
+++ app/code/local/Symfony/Core/Block/Template.php
@@ -11,21 +11,38 @@ use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
  * @author Robert Gruendler <r.gruendler@gmail.com>
  *
  */
-class Symfony_Core_Block_Template extends Mage_Core_Block_Template {
+class Symfony_Core_Block_Template extends Mage_Core_Block_Template implements Twig_ExtensionInterface {
 
     
     /** @var Symfony\Bundle\FrameworkBundle\Templating\EngineInterface $engine */
     protected $engine;
     
     public function __construct() {
-
+        
+        parent::_construct();
+        
         $container = \Mage::getSingleton('Symfony_Core_DependencyInjection_Container');
         $this->engine = $container->get('templating');
         
         if (!$this->engine instanceof EngineInterface) {            
             throw new \Exception("Unable to load Symfony templating engine");            
         }
+                
+        $twig = $container->get('twig');        
         
+        if ($twig instanceof  \Twig_Environment) {
+            
+            $twig->addExtension($this);
+            $methods = get_class_methods($this);
+            foreach ($methods as $method)
+            {
+                if('TwigExtension' == substr($method,-13))
+                {
+                    $function = substr($method, 0, strlen($method) - 13);
+                    $twig->addFunction($function, new Twig_Function_Method($this,$method));
+                }
+            }
+        }        
     }
     
     
@@ -41,6 +58,7 @@ class Symfony_Core_Block_Template extends Mage_Core_Block_Template {
 
         Varien_Profiler::start($fileName);
         
+
         extract ($this->_viewVars);
         $do = $this->getDirectOutput();
                 
@@ -57,7 +75,6 @@ class Symfony_Core_Block_Template extends Mage_Core_Block_Template {
         
         try {            
             echo $this->engine->render($fileName, $this->_viewVars);
-            
         } catch (Exception $e) {
             ob_get_clean();
             throw $e;
@@ -77,4 +94,97 @@ class Symfony_Core_Block_Template extends Mage_Core_Block_Template {
         return $html;        
         
     }
+    
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'TwigExtension-'.get_class($this);
+    }
+    
+    /**
+     * Initializes the runtime environment.
+     *
+     * This is where you can load some file that contains filter functions for instance.
+     *
+     * @param Twig_Environment $environment The current Twig_Environment instance
+     */
+    public function initRuntime(Twig_Environment $environment)
+    {
+    }
+    
+    /**
+     * Returns the token parser instances to add to the existing list.
+     *
+     * @return array An array of Twig_TokenParserInterface or Twig_TokenParserBrokerInterface instances
+     */
+    public function getTokenParsers()
+    {
+        return array();
+    }
+    
+    /**
+     * Returns the node visitor instances to add to the existing list.
+     *
+     * @return array An array of Twig_NodeVisitorInterface instances
+     */
+    public function getNodeVisitors()
+    {
+        return array();
+    }
+    
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return array An array of filters
+     */
+    public function getFilters()
+    {
+        return array();
+    }
+    
+    /**
+     * Returns a list of tests to add to the existing list.
+     *
+     * @return array An array of tests
+     */
+    public function getTests()
+    {
+        return array();
+    }
+    
+    /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return array An array of functions
+     */
+    public function getFunctions()
+    {
+        return array();
+    }
+    
+    /**
+     * Returns a list of operators to add to the existing list.
+     *
+     * @return array An array of operators
+     */
+    public function getOperators()
+    {
+        return array();
+    }
+    
+    /**
+     * Returns a list of global functions to add to the existing list.
+     *
+     * @return array An array of global functions
+     */
+    public function getGlobals()
+    {
+        return array();
+    }
+    
+    
 }
